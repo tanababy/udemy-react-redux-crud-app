6def826a7f51218a6c1ca283b003084f3dfe3d45
tanabee8 <tanabe-y@dentsu-crx.co.jp>
2020-04-02 01:52:08 +0900

    完全に理科下

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import React, { Component } from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="for">
-//           <input type="text" onChange={()=>{console.log('momoko!');}} />
-//         </label>
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 
 const App = () => (<Counter></Counter>)
 
@@ -41,15 +29,5 @@ class Counter extends Component {
 
   }
 }
-
-// class App extends Component {
-//   render() {
-//     return React.createElement(
-//       "div",
-//       null,
-//       "Ozono Momoko!!"
-//     )
-//   }
-// }
 
 export default App;
---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+export const increment = () => ({
+    type: INCREMENT
+});
+
+export const increment = () => ({
+    type: DECREMENT
+});
