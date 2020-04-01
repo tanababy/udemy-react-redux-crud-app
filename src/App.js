c9eb5c41712667febbc2063a12a57ae01ce9c9b5
tanabee8 <tanabe-y@dentsu-crx.co.jp>
2020-04-01 19:10:35 +0900

    nice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="for">
-          <input type="text" onChange={()=>{console.log('momoko!');}} />
-        </label>
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="for">
+//           <input type="text" onChange={()=>{console.log('momoko!');}} />
+//         </label>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const Kakki = () => {
+  return <div>i see!</div>
 }
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Kakki />
+      <Kakki />
+      <Kakki />
+      <Kakki />
+    </React.Fragment>
+  )
+}
+
 
 // class App extends Component {
 //   render() {
